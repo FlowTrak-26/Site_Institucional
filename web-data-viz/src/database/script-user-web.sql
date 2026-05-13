eb0e80b5a51d19acaefa083a02a03e6e26f60854
victor-sousao <victor.soliveira@sptech.school>
2026-05-13 02:31:32 -0300

    Atualizando BD da API

--- web-data-viz/src/database/script-user-web.sql
+++ web-data-viz/src/database/script-user-web.sql
@@ -1,2 +1,2 @@
-CREATE USER 'user_web_data_viz'@'%' IDENTIFIED BY 'SPTech#2024';
-GRANT ALL PRIVILEGES ON flowtrak.* TO user_web_data_viz'@'%';
+CREATE USER 'user_admin'@'%' IDENTIFIED BY 'SPTech#2024';
+GRANT ALL PRIVILEGES ON flowtrak.* TO user_admin'@'%';
