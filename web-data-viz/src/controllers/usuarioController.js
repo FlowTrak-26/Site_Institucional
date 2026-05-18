4226a7b65cbc1ccf10a25143b6cba64e80bcf8b8
victor-sousao <victor.soliveira@sptech.school>
2026-05-17 23:30:23 -0300

    Removendo telefone do controller

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -50,8 +50,8 @@ function cadastrar(req, res) {
 	var nome = req.body.nomeServer;
 	var email = req.body.emailServer;
 	var senha = req.body.senhaServer;
-	var telefone = req.body.telefoneServer;
-	var fk_empresa_parceira = req.body.idEmpresaVincularServer;
+	// var telefone = req.body.telefoneServer;
+	var fk_empresa_parceira = req.body.empresaServer;
 
 	// Faça as validações dos valores
 	if (nome == undefined) {
@@ -62,12 +62,16 @@ function cadastrar(req, res) {
 		res.status(400).send("Sua senha está undefined!");
 	} else if (fk_empresa_parceira == undefined) {
 		res.status(400).send("Sua empresa a vincular está undefined!");
-	} else if (telefone == undefined) {
-		res.status(400).send("Seu telefone está undefined");
+		// } else if (telefone == undefined) {
+		// 	res.status(400).send("Seu telefone está undefined");
 	} else {
 
 		// Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-		usuarioModel.cadastrar(nome, email, senha, telefone, fk_empresa_parceira)
+		usuarioModel.cadastrar(nome,
+			email,
+			senha,
+			//    telefone,
+			fk_empresa_parceira)
 			.then(
 				function (resultado) {
 					res.json(resultado);
